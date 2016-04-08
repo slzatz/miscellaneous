8685336b1a446eb93c348435415a40fbec240831
Steve Zatz <slzatz@gmail.com>
2016-04-07 21:04:41 -0400

    These scripts are for displaying the artist and song on the FeatherWing OLED

---
+++ esp8266/scrobble_scripts/Readme.md
@@ -0,0 +1,1 @@
+These scripts placed in the scripts directory of esp8266 display the song playing on Sonos.
---
+++ esp8266/scrobble_scripts/main_scrobble.py
@@ -0,0 +1,44 @@
+from time import sleep
+import socket
+import json
+import network
+from config import host, ssid, pw
+from ssd1306_min import SSD1306 as ssd
+
+d = ssd()
+d.init_display()
+d.draw_text(0, 0, "HELLO STEVE")
+d.display()
+
+def run():
+  wlan = network.WLAN(network.STA_IF)
+  wlan.active(True)
+  if not wlan.isconnected():
+    print('connecting to network...')
+    wlan.connect(ssid, pw)
+    while not wlan.isconnected():
+      pass
+  print('network config:', wlan.ifconfig())     
+
+  while 1:
+      s = socket.socket()
+      s.connect((host, 5000))
+      s.send('GET /sonos_check HTTP/1.0\r\n\r\n')
+      z = s.recv(64)
+      z = s.recv(1024)
+      s.close()
+      zz = z.decode('ascii', 'replace')
+      i = zz.find('{')
+      zzz = json.loads(zz[i:])
+
+      if zzz['updated']:
+        d.clear()
+        d.display()
+        d.draw_text(0, 0, zzz['artist'][:20])
+        d.draw_text(0, 12, zzz['title'][:20])
+        d.draw_text(0, 24, zzz['title'][20:])
+        d.display()
+
+      sleep(3)
+
+run()

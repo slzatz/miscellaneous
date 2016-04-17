5d3345943d6901d8e1ea725d58c339a272f7f451
slzatz <slzatz@gmail.com>
2016-04-17 19:27:35 -0400

    ssd1306_min2.py: bug fix

--- esp8266/scrobble_scripts/ssd1306_min2.py
+++ esp8266/scrobble_scripts/ssd1306_min2.py
@@ -48,7 +48,7 @@ class SSD1306:
             SET_MUX_RATIO, 31, #self.height - 1,
             SET_COM_OUT_DIR | 0x08, # scan from COM[N] to COM0
             SET_DISP_OFFSET, 0x00,
-            SET_COM_PIN_CFG, 0x02 #if self.height == 32 else 0x12,
+            SET_COM_PIN_CFG, 0x02, #if self.height == 32 else 0x12,
             # timing and driving scheme
             SET_DISP_CLK_DIV, 0x80,
             SET_PRECHARGE, 0xf1, #0x22 if self.external_vcc else 0xf1,

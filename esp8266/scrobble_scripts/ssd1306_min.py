013194b529989f2ea2f669bf960575ac8382d840
slzatz <slzatz@gmail.com>
2016-04-18 08:26:41 -0400

    ssd1306_min.py: cleanup of draw_text since size and space always = 1

--- esp8266/scrobble_scripts/ssd1306_min.py
+++ esp8266/scrobble_scripts/ssd1306_min.py
@@ -22,14 +22,13 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from machine import Pin, I2C
+#from machine import Pin, I2C
 import font
 
 # Constants
 DISPLAYOFF          = const(0xAE)
 SETCONTRAST         = const(0x81)
 DISPLAYALLON_RESUME = const(0xA4)
-#DISPLAYALLON        = const(0xA5)
 NORMALDISPLAY       = const(0xA6)
 INVERTDISPLAY       = const(0xA7)
 DISPLAYON           = const(0xAF)
@@ -39,38 +38,24 @@ SETVCOMDETECT       = const(0xDB)
 SETDISPLAYCLOCKDIV  = const(0xD5)
 SETPRECHARGE        = const(0xD9)
 SETMULTIPLEX        = const(0xA8)
-#SETLOWCOLUMN        = const(0x00)
-#SETHIGHCOLUMN       = const(0x10)
 SETSTARTLINE        = const(0x40)
 MEMORYMODE          = const(0x20)
 COLUMNADDR          = const(0x21)
 PAGEADDR            = const(0x22)
-#COMSCANINC          = const(0xC0)
 COMSCANDEC          = const(0xC8)
 SEGREMAP            = const(0xA0)
 CHARGEPUMP          = const(0x8D)
-#EXTERNALVCC         = const(0x10)
-#SWITCHCAPVCC        = const(0x20)
-#SETPAGEADDR         = const(0xB0)
-#SETCOLADDR_LOW      = const(0x00)
-#SETCOLADDR_HIGH     = const(0x10)
-#ACTIVATE_SCROLL                      = const(0x2F)
-#DEACTIVATE_SCROLL                    = const(0x2E)
-#SET_VERTICAL_SCROLL_AREA             = const(0xA3)
-#RIGHT_HORIZONTAL_SCROLL              = const(0x26)
-#LEFT_HORIZONTAL_SCROLL               = const(0x27)
-#VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = const(0x29)
-#VERTICAL_AND_LEFT_HORIZONTAL_SCROLL  = const(0x2A)
 
-class SSD1306(object):
+class SSD1306:
 
-  def __init__(self):
+  def __init__(self, i2c):
     # assumes height=32, pages=4, columns=128, internal vcc
 
     self.cbuffer = bytearray(2)
     self.cbuffer[0] = 0x80
 
-    self.i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000) 
+    #self.i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000) 
+    self.i2c = i2c
 
   def clear(self):
     self.buffer = bytearray(513)
@@ -93,6 +78,7 @@ class SSD1306(object):
     self.i2c.writeto(0x3c, self.buffer)
 
   def set_pixel(self, x, y, state):
+    # each byte of buffer covers 8 pixels and index gets you the position in the buffer
     index = x + (int(y / 8) * 128)
     if state:
       self.buffer[1 + index] |= (1 << (y & 7))
@@ -100,11 +86,6 @@ class SSD1306(object):
       self.buffer[1 + index] &= ~(1 << (y & 7))
 
   def init_display(self):
-    #chargepump = 0x14
-    #precharge  = 0xf1
-    #multiplex  = 0x1f 
-    #compins    = 0x02 
-    #contrast   = 0xff # 0x8f if self.height == 32 else (0x9f if self.external_vcc else 0x9f)
     data = [DISPLAYOFF,
             SETDISPLAYCLOCKDIV, 0x80,
             SETMULTIPLEX, 0x1f,
@@ -136,30 +117,29 @@ class SSD1306(object):
     self.write_command(SETCONTRAST)
     self.write_command(contrast)
 
-  def draw_text(self, x, y, string, size=1, space=1):
-    def pixel_x(char_number, char_column, point_row):
-      char_offset = x + char_number * size * font.cols + space * char_number
-      pixel_offset = char_offset + char_column * size + point_row
+  def draw_text(self, x, y, string): #, size=1, space=1):
+    def pixel_x(char_number, char_column):
+      char_offset = x + char_number * font.cols + char_number
+      pixel_offset = char_offset + char_column + 1
       return 128 - pixel_offset
 
-    def pixel_y(char_row, point_column):
-      char_offset = y + char_row * size
-      return char_offset + point_column
+    def pixel_y(char_row):
+      char_offset = y + char_row 
+      return char_offset 
 
     def pixel_mask(char, char_column, char_row):
-      #char_index_offset = ord(char) * font.cols #original
+      # To save space, eliminated first 32 non-printing chars
       char_index_offset = (ord(char)-32) * font.cols
       return font.bytes[char_index_offset + char_column] >> char_row & 0x1
 
     pixels = (
-      (pixel_x(char_number, char_column, point_row),
-       pixel_y(char_row, point_column),
-       pixel_mask(char, char_column, char_row))
-      for char_number, char in enumerate(string)
-      for char_column in range(font.cols)
-      for char_row in range(font.rows)
-      for point_column in range(size)
-      for point_row in range(1, size + 1))
+             (pixel_x(char_number, char_column),
+              pixel_y(char_row),
+              pixel_mask(char, char_column, char_row))
+              for char_number, char in enumerate(string)
+              for char_column in range(font.cols)
+              for char_row in range(font.rows)
+              )
 
-    for pixel in pixels:
-      self.set_pixel(*pixel)
+    for pixl in pixels:
+      self.pixel(*pixl)

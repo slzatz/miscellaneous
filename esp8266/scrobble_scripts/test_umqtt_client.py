60ddf81c529e0cf9e80d597c1470392d5f7a9e68
slzatz <slzatz@gmail.com>
2016-05-11 21:20:03 -0400

    umqtt_client.py and test_umqtt_client.py - updates handles both ping response and receiving subscribed messages

--- esp8266/scrobble_scripts/test_umqtt_client.py
+++ esp8266/scrobble_scripts/test_umqtt_client.py
@@ -1,4 +1,20 @@
-from umqtt import MQTTClient as m
+'''
+server = 54.173.234.69
+54.173.234.69 1883
+connect
+b' \x02\x00\x00'
+subscribe
+b'\x90\x03\x00\x01\x00'
+1462978855.068099
+res =  b'1'
+(b'test', b'This is a new message')
+1462978856.0821
+1462978857.096102
+1462978858.096319
+Traceback (most recent call last):
+'''
+
+from umqtt_client import MQTTClient as m
 import time
 
 c = m('abc', '54.173.234.69')
@@ -9,11 +25,13 @@ while 1:
     z = c.check_msg()
     if z:
         print(z)
-    #print(c.check_msg())
-    #print(time.time())
+        if isinstance(z, int):
+            print("returned a command")
+        else:
+            print("returned a tuple")
     t = time.time()
     print(t)
     if t > cur_time + 10:
-        print(c.ping())
+        c.ping()
         cur_time = t
     time.sleep(1)
--- esp8266/scrobble_scripts/umqtt_client.py
+++ esp8266/scrobble_scripts/umqtt_client.py
@@ -25,10 +25,12 @@ class MQTTClient:
         self.pid = 0
 
     def send_str(self, s):
+        # H = use two bytes to represent number
         self.sock.send(struct.pack("!H", len(s)))
         self.sock.send(s)
 
     def connect(self):
+        print("connect")
         self.sock.connect(self.addr)
         msg = bytearray(b"\x10\0\0\x04MQTT\x04\x02\0\0")
         msg[1] = 10 + 2 + len(self.client_id)
@@ -37,6 +39,7 @@ class MQTTClient:
         self.send_str(self.client_id)
         resp = self.sock.recv(4)
         assert resp == b"\x20\x02\0\0", resp
+        print(resp)
 
     def disconnect(self):
         self.sock.send(b"\xe0\0")
@@ -53,8 +56,10 @@ class MQTTClient:
         self.sock.send(msg.encode('utf-8'))
 
     def subscribe(self, topic):
+        print("subscribe")
         pkt = bytearray(b"\x82\0\0\0")
         self.pid += 1
+        # B=1 byte; H = 2 bytes,!=network or big endian and offset is 1
         struct.pack_into("!BH", pkt, 1, 2 + 2 + len(topic) + 1, self.pid)
         #print(hex(len(pkt)), hexlify(pkt, ":"))
         self.sock.send(pkt)
@@ -66,49 +71,61 @@ class MQTTClient:
         assert resp[2] == pkt[2] and resp[3] == pkt[3]
         assert resp[4] == 0
 
-    def wait_msg(self):
+    def check_msg(self):
+        self.sock.setblocking(False)
         try:
             res = self.sock.recv(1) #read(1)
-            print("res = ",res)
+            #print("res = ",res)
             #print(bin(res))
         except:
             #print("Exception: No data")
             return None
         #if res[0] >> 4 !=3: #more general but not handling QoS > 0
-        if res[0] not in (48,49):
+        # should now add the ping response and then there shouldn't be
+        # collision between server sending ping response and a new msg
+        if res[0] in (48,49):
+            self.sock.setblocking(True)
+            sz = self.sock.recv(1)[0]
+            if sz > 127:
+                sz1 = self.sock.recv(1)[0]
+                sz = sz1*128 + sz - 128
+
+            z = self.sock.recv(sz)
+            # topic length is first two bytes of variable header
+            # it's just a 16 bit integer
+            # first byte is bits 9 - 16
+            # So that is shifting the first bit 8 places
+            # the second bit is just the second bit
+            topic_len = z[:2]
+            topic_len = (topic_len[0] << 8) | topic_len[1]
+            topic = z[2:topic_len+2]
+            msg = z[topic_len+2:]
+            return (topic, msg)
+
+        elif res[0]>>4==13:
+            self.sock.setblocking(True)
+            s = self.sock.recv(1) # second byte should be 0
+            return 13
+
+        else:
             return res
-        self.sock.setblocking(True)
-        sz = self.sock.recv(1)[0]
-        if sz > 127:
-            sz1 = self.sock.recv(1)[0]
-            sz = sz1*128 + sz - 128
 
-        z = self.sock.recv(sz)
-        # topic length is first two bytes of variable header
-        # it's just a 16 bit integer
-        # first byte is bits 9 - 16
-        # So that is shifting the first bit 8 places
-        # the second bit is just the second bit
-        topic_len = z[:2]
-        topic_len = (topic_len[0] << 8) | topic_len[1]
-        topic = z[2:topic_len+2]
-        msg = z[topic_len+2:]
-        return (topic, msg)
-
-    def check_msg(self):
-        self.sock.setblocking(False)
-        return self.wait_msg()
+    #def check_msg(self):
+    #    self.sock.setblocking(False)
+    #    return self.wait_msg()
 
     def ping(self):
         pkt = bytearray([0b11000000,0x0])
         self.sock.send(pkt)
-        time.sleep(1)
+
+    def read_command():
         self.sock.setblocking(False)
         try:
-            res = self.sock.recv(2) #read(1)
-            print("res = ",res)
-            return res[0]>>4
+            res = self.sock.recv(1) #read(1)
+            #print("res = ",res)
             #print(bin(res))
         except:
-            print("Exception: No data")
+            #print("Exception: No data")
             return None
+        else:
+            return res[0]

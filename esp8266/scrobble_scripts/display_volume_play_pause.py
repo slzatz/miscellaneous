3699e8eee2e771f8a171983aa1a255f15d39b59d
slzatz <slzatz@gmail.com>
2016-07-01 21:17:30 -0400

    display_volume_play_pause.py: several small changes mainly around using a location and an mqtt_id file that are read by the script

--- esp8266/scrobble_scripts/display_volume_play_pause.py
+++ esp8266/scrobble_scripts/display_volume_play_pause.py
@@ -1,25 +1,32 @@
 '''
-This micropython script is for use with the FeatherWing doubler.  It displays songs that
-are being scrobbled to the mqtt broker running in AWS EC2 and also the top bottom (A) increases the volume, 
-the bottom button (C) decreases the volume and middle (B) play_pauses
+This micropython script is for use with the hacked FeatherWing doubler.  It displays songs that
+are being scrobbled to the mqtt broker running in AWS EC2 or locally and also the top bottom (A)
+increases the volume, the bottom button (C) decreases the volume and middle (B) play_pauses
+The separate button on GPIO 14 also play_pauses.
 The script also pings the broker to keep it alive
-The other board includes a volume potentiometer and a play/pause button
+One board is the FeatherWing SSD1306 OLED and the other has a play/pause button and potentiometer
 '''
 
 import gc
 from time import sleep, time
 import json
 import network
-from config import host, ssid, pw, mqtt_id, port
+from config import hosts, ssid, pw 
 from ssd1306_min import SSD1306 as SSD
 from umqtt_client import MQTTClient as umc
 from machine import Pin, I2C, ADC
-try:
-  from location import loc
-except ImportError:
-  from config import loc
 
-print("location =",loc)
+with open('mqtt_id', 'r') as f:
+    mqtt_id = f.read().strip()
+
+with open('location', 'r') as f:
+    loc = f.read().strip()
+
+host = hosts[loc]
+
+print("mqtt_id =", mqtt_id)
+print("location =", loc)
+print("host =", host)
 
 i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000)
 
@@ -28,7 +35,7 @@ d.init_display()
 d.draw_text(0, 0, "HELLO STEVE")
 d.display()
 
-c = umc(mqtt_id, host, port=port)
+c = umc(mqtt_id, host, 1883)
 
 b = bytearray(1)
 # mtpPublish is a class method that produces a bytes object that is used in
@@ -95,7 +102,6 @@ def run():
       pass
   print('network config:', wlan.ifconfig())     
 
-  #c = umc('abc', host)
   c.connect()
   c.subscribe('sonos/{}/current_track'.format(loc))
   sleep(2) 
@@ -106,7 +112,7 @@ def run():
 
   while 1:
     new_level = adc.read()
-    if abs(new_level-level) > 50:
+    if abs(new_level-level) > 20:
       try:
         c.publish('sonos/'+loc, json.dumps({"action":"volume", "level":new_level}))
       except Exception as e:
